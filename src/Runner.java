8d5ca9352912b844045c5f5a90b61f223d93a6e7
BT_1E10_23 <sample@bths.edu>
2022-03-11 13:20:25 -0500

    Final

--- src/Runner.java
+++ src/Runner.java
@@ -5,14 +5,18 @@ public class Runner {
 //        Encryptor encryptor2 = new Encryptor(6, 5);
 //        System.out.println(encryptor2.encryptMessage("This one time I was trying to do a handstand and fell backwards and landed on my shoe. It hurt! I won't try that again."));
 
-        Encryptor decrypto1 = new Encryptor(3, 2);
-        System.out.println(decrypto1.decryptMessage("Ti hsiscn ofietdnia!Bl e nto helo okotAu!A"));
-        Encryptor decrypto2 = new Encryptor(5, 5);
-        System.out.println(decrypto2.decryptMessage("Tite hsocmi presa es  stsaneregtd see J  nfaBidomonee  d ftm.cuhe alisBr,ssaieecgllstelft!  -rAwsduA"));
-        Encryptor decrypto3 = new Encryptor(6, 5);
-        System.out.println(decrypto3.decryptMessage("Meottai ooufdsl rtdcsrnelh e r etWwht hhiihmselgro.nlhen    sld tmc egoehsne  otttcbous oacHusAAkoloAA?ploAA eynAA f !AA"));
-        Encryptor decrypto4 = new Encryptor(9, 8);
-        System.out.println(decrypto4.decryptMessage("Io ihscnongeoesofn rnn  do.cac poir rprirfnmTyhyso gahpyp ce tit,tteniis  siea o,pc gpttr rotirihmkonhneoeancveasn toee le iiwsro nononstrrtffn atttencxlseoe oitl x rfwp.yptanonh ,l,nar eI a  tmardoiiai stennnlvk ealyit real  zid rcaaaeeeatkinudsc e ndt  icxtt hpcpitoeaoaahp  xcrrnehbptceo tc t  sriird pisinoyoirn gfnpetettio.tssvehnr i eereamEonlnf lannoftert eetoite tl  unondhlctltrceeioode.en gn -r  iiitabcFbenbe eeoustlnw pr annn  patlsc uusne ,rsssedcr ycueuoheaphasdmnantel o is imla-ecoeoey rnc eyoIsotrkr rts  yeaai idtpytntibeht e hslceigdam er oe l.p ymnnbg otpests  r-dtshstb d iaoehuaeeoguset snnets ,wic   ik egrswpneflnyciogyolephesearsi dmi te lr.edcisale ,eooonsqA rmnud uncapar ai obulcsrranlt ekeeut naceehrhrtsr  eooe iymw vrcclpeikiiiaytstedzpn  shyeei dta  ddeeehgtp brribthuAyi puoosA gtit reAtioe uirAhn nnnzsAeartoae.A testudAAooc  t AA"));
+//        Encryptor decrypto1 = new Encryptor(3, 2);
+//        System.out.println(decrypto1.decryptMessage("Ti hsiscn ofietdnia!Bl e nto helo okotAu!A"));
+//        Encryptor decrypto2 = new Encryptor(5, 5);
+//        System.out.println(decrypto2.decryptMessage("Tite hsocmi presa es  stsaneregtd see J  nfaBidomonee  d ftm.cuhe alisBr,ssaieecgllstelft!  -rAwsduA"));
+//        Encryptor decrypto3 = new Encryptor(6, 5);
+//        System.out.println(decrypto3.decryptMessage("Meottai ooufdsl rtdcsrnelh e r etWwht hhiihmselgro.nlhen    sld tmc egoehsne  otttcbous oacHusAAkoloAA?ploAA eynAA f !AA"));
+//        Encryptor decrypto4 = new Encryptor(9, 8);
+//        System.out.println(decrypto4.decryptMessage("Io ihscnongeoesofn rnn  do.cac poir rprirfnmTyhyso gahpyp ce tit,tteniis  siea o,pc gpttr rotirihmkonhneoeancveasn toee le iiwsro nononstrrtffn atttencxlseoe oitl x rfwp.yptanonh ,l,nar eI a  tmardoiiai stennnlvk ealyit real  zid rcaaaeeeatkinudsc e ndt  icxtt hpcpitoeaoaahp  xcrrnehbptceo tc t  sriird pisinoyoirn gfnpetettio.tssvehnr i eereamEonlnf lannoftert eetoite tl  unondhlctltrceeioode.en gn -r  iiitabcFbenbe eeoustlnw pr annn  patlsc uusne ,rsssedcr ycueuoheaphasdmnantel o is imla-ecoeoey rnc eyoIsotrkr rts  yeaai idtpytntibeht e hslceigdam er oe l.p ymnnbg otpests  r-dtshstb d iaoehuaeeoguset snnets ,wic   ik egrswpneflnyciogyolephesearsi dmi te lr.edcisale ,eooonsqA rmnud uncapar ai obulcsrranlt ekeeut naceehrhrtsr  eooe iymw vrcclpeikiiiaytstedzpn  shyeei dta  ddeeehgtp brribthuAyi puoosA gtit reAtioe uirAhn nnnzsAeartoae.A testudAAooc  t AA"));
+//        Encryptor decyrpto5 = new Encryptor(10, 15);
+//        System.out.println(decyrpto5.decryptMessage("Mnos dgvAAygr hareAA  ntayerAAccihp ayAAarnep:toAAtagyy) nAAsz!   weAA y ai e!AAa  rtHeAAArtIe'akAAAeh  sveAAA iga enAAAgsulF dAAAo esra AAAimsoi eAAA"));
 
+        Encryptor decryptor = new Encryptor(3, 5);
+        System.out.println(decryptor.decryptMessage("I isMlnrl’.etMr BAtEPhS eTC  SAcA hAteAerAa?A"));
     }
 }
